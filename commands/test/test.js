50759eec8c87d5d04a67426747e19af65f29332b
Anthony Legrain <legrain.anthony51@gmail.com>
2023-04-21 00:05:47 +0200

    new command for 1v1 lol

--- commands/test/test.js
+++ commands/test/test.js
@@ -1,7 +1,20 @@
+import championsDatas from '../../data/rawData/champions.json' assert {type: 'json'}
+
 export default {
-    name: 'test',
+    name: '1v1',
     help: false,
     execute(message) {
-      message.channel.send('test reussi');
+      const nameChamps = []; 
+      for (const champ in championsDatas.data) {
+       nameChamps.push(champ);
+       console.log(champ);
+      }
+      let firstRandomValue = Math.floor(Math.random() * nameChamps.length);
+      let firstChamp = nameChamps[firstRandomValue];
+
+      let secondRandomValue = Math.floor(Math.random() * nameChamps.length);
+      let secondChamp = nameChamps[secondRandomValue];
+
+      message.reply(`Vous allez jouer ${firstChamp} contre ${secondChamp}`)
     },
   };
