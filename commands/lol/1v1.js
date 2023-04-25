dbfcb28005074c9861e58e191f347c67a91e443d
Legrain Anthony <legrain.anthony51@gmail.com>
2023-04-25 16:46:30 +0200

    Revert "need pull"
    This reverts commit bb4115bef823cf2062dadad21b2108443a2089f1.

--- commands/lol/1v1.js
+++ commands/lol/1v1.js
@@ -1,11 +1,14 @@
-import getAllNamesChamps from '../../utils/helpers/lol/allChamp.js'
+import championsDatas from '../../data/rawData/champions.json' assert {type: 'json'}
 
 export default {
     name: '1v1',
     help: false,
     execute(message) {
-      const nameChamps = getAllNamesChamps();
-      
+      const nameChamps = []; 
+      for (const champ in championsDatas.data) {
+       nameChamps.push(champ);
+       console.log(champ);
+      }
       let firstRandomValue = Math.floor(Math.random() * nameChamps.length);
       let firstChamp = nameChamps[firstRandomValue];
 
