de580f589622df909db8a844d45de7a959269000
Legrain Anthony <legrain.anthony51@gmail.com>
2023-06-06 13:58:42 +0200

    trad

--- commands/lol/hide.js
+++ commands/lol/hide.js
@@ -6,12 +6,51 @@ export default {
     help: false,
     description: 'aaa',
     async execute(message, client, args) {
-
+                class Map {
+            constructor(canvas, ctx, background) {
+              this.canvas = canvas;
+              this.ctx = ctx;
+              this.background = background;
+            }
+          
+            async draw() {
+              const attachment = new Discord.MessageAttachment(await this.canvas.encode('png'), 'map.png');
+              message.channel.send({ files: [attachment] });
+            }
+          }
+          
+          async function mapWithPlayers(players) {
+            const canvas = Canvas.createCanvas(500, 500);
+            const ctx = canvas.getContext('2d');
+            const background = await Canvas.loadImage('/home/legrain/Bureau/GOOFY_AHHH_BOT/assets/images/map11.png');
+          
+            // Dessiner l'image de fond
+            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+          
+            // Dessiner les cercles des joueurs
+            for (const player of players) {
+              console.log(player);
+              const avatarUrl = player.avatar;
+              const avatar = await Canvas.loadImage(avatarUrl);
+              ctx.beginPath();
+              ctx.arc(player.x + 15, player.y + 15, 15, 0, 2 * Math.PI);
+              ctx.closePath();
+              ctx.clip();
+              ctx.drawImage(avatar, player.x, player.y, 30, 30);
+              ctx.strokeStyle = 'white';
+              ctx.lineWidth = 2;
+              ctx.stroke();
+              ctx.restore(); // Ajoutez cette ligne pour restaurer le contexte initial
+            }
+          
+            new Map(canvas, ctx, background).draw();
+          }
+          
         async function createFirstMap() {
             const canvas = Canvas.createCanvas(500, 500);
             const ctx = canvas.getContext('2d');
             const background = await Canvas.loadImage('/home/legrain/Bureau/GOOFY_AHHH_BOT/assets/images/map11.png');
-    
+
             // Draw the background first
             ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     
@@ -70,18 +109,44 @@ export default {
             
             const responsesObjects = []
             const potitions = message.channel.awaitMessages({time: 10000 })
-                potitions.then((responses) => {
+                potitions.then((responses) => {             
                 responses.forEach((response) => {
-                    responsesObjects.push({name: response.author.username, content : response.content})
+                    responsesObjects.push({name: response.author.username, content : response.content, avatar: message.author.avatarURL()})
                 })
             }).then(() => {
-                console.log(responsesObjects);
-            })
-
+                const lastValues = {};
+                const goodFormat = /\b(x|y):\d{1,2}\b/
+              
+                for (const response of responsesObjects) {
+                  if (goodFormat.test(response.content)) {
+                    lastValues[response.name] = {
+                      content: response.content,
+                      avatar: response.avatar // inclure la propriété avatar dans la structure de données
+                    };
+                  }
+                }
+              
+                const result = Object.entries(lastValues).map(([name, value]) => ({
+                  name,
+                  content: value.content,
+                  avatar: value.avatar // récupérer la propriété avatar depuis la structure de données
+                }));
+              
+                const players = result.map(element => {
+                  const contentSplit = element.content.split(' ');
+                  const x = Number(contentSplit[0].split(':')[1] * 10);
+                  const y = Number(contentSplit[1].split(':')[1] * 10);
+                  return { name: element.name, x: x, y: y, avatar: element.avatar };
+                });
+              
+                if (players.length > 0) {
+                  mapWithPlayers(players)
+                } else {
+                  message.channel.send('Il n\'y a pas de joueurs')
+                }
+              })
         }
 
-
-
         createFirstMap();
-    },
+     },
 };
--- commands/test/test.js
+++ commands/test/test.js
@@ -1,7 +1,34 @@
+ import axios from 'axios';
+
 export default {
-    name: 'test',
-    help: false,
-    execute(message) {
-      message.channel.send('test reussi');
-    },
+  name: 'nazi',
+  help: false,
+   async execute(message, client, args) {
+
+    const str = args.join(' ');
+
+        const encodedParams = new URLSearchParams();
+        encodedParams.set('source', 'fr');
+        encodedParams.set('target', 'de');
+        encodedParams.set('q', `${str}`);
+
+        const options = {
+          method: 'POST',
+          url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Accept-Encoding': 'application/gzip',
+            'X-RapidAPI-Key': 'de65c214b9msh3ad6642938dc20cp184046jsn7595a3d4e88c',
+            'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+          },
+          data: encodedParams,
+        };
+
+        try {
+          const response = await axios.request(options);
+          message.reply(response.data.data.translations[0].translatedText);
+        } catch (error) {
+          console.error(error);
+        }
+      },
   };
